83e236019393700acfde2c09a5d8c11188f690c4
likwotsing <likwotsing@gmail.com>
2019-05-16 21:55:43 +0800

    :car: head代码

--- src/main.js
+++ src/main.js
@@ -2,8 +2,12 @@ import Vue from 'vue'
 import fastClick from 'fastclick'
 import App from './App.vue'
 import router from './router'
-import '@/assets/styles/reset.css'
-import '@/assets/styles/border.css'
+// import '@/assets/styles/reset.css'
+// import '@/assets/styles/border.css'
+// import '@/assets/styles/iconfont.css'
+import 'styles/reset.css'
+import 'styles/border.css'
+import 'styles/iconfont.css'
 
 Vue.config.productionTip = false
 fastClick.attach(document.body)
---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="header">
+    <div class="header-left">
+      <span class="iconfont icon-fanhui"></span>
+    </div>
+    <div class="header-input">
+      <span class="iconfont icon-sousuo"></span>
+      输入城市/景点/游玩主题
+    </div>
+    <div class="header-right">
+      城市
+      <span class="iconfont icon-jiantou arrow-icon"></span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+  // @import '~@/assets/styles/variables.styl'
+  @import '~styles/variables.styl'
+  .header
+    display: flex
+    line-height: .86rem
+    background: $bgColor
+    .header-left
+      width: .64rem
+      float: left
+      text-align: center
+    .header-input
+      flex: 1
+      height: .64rem
+      line-height .64rem
+      margin-top: .12rem
+      margin-left: .6rem
+      padding-left: .2rem
+      background: #ffffff
+      border-radius: .1rem
+      color: #ccc
+    .header-right
+      width 1.24rem
+      float: right
+      text-align: center
+      .arrow-icon
+        margin-left: -0.04rem
+        font-size: .24rem
+</style>
